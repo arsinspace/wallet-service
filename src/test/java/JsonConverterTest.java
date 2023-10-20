f161dbc0f2b6159a12590c0ddb6ba12a38e2a6fc
arsinspace <140083535+arsinspace@users.noreply.github.com>
2023-10-20 13:48:16 +0300

    Rework with Mockito and AssertJ JsonConverterTest.java

--- src/test/java/JsonConverterTest.java
+++ src/test/java/JsonConverterTest.java
@@ -1,8 +1,12 @@
-import org.junit.Assert;
-import org.junit.Test;
-import ru.ylab.model.Credentials;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import ru.ylab.model.User;
 import ru.ylab.utils.JsonConverter;
+import org.assertj.core.api.Assertions;
+
+import java.util.Objects;
+
+import static org.mockito.Mockito.when;
 
 /**
  * Tests for JsonConverter
@@ -13,14 +17,11 @@ public class JsonConverterTest {
     public void convertToObjectShouldBeReturnValidObject(){
         String testJsonUser = "{\"name\":\"Adam\",\"lastName\":\"Adam\",\"age\":\"29\",\"credentials\"" +
                 ":{\"login\":\"adam\",\"password\":\"123\"}}";
-        User testUser = User.builder()
-                .name("Adam")
-                .lastName("Adam")
-                .age(String.valueOf(29))
-                .credentials(new Credentials("adam","123"))
-                .build();
 
-        Assert.assertEquals(JsonConverter.convertToObject(User.class,testJsonUser).getCredentials().toString(),
-                testUser.getCredentials().toString());
+        User testUser = Mockito.mock(User.class);
+        when(testUser.getAge()).thenReturn("29");
+
+        Assertions.assertThat(Objects.requireNonNull(JsonConverter.convertToObject(User.class, testJsonUser))
+                .getAge()).isEqualTo(testUser.getAge());
     }
 }
