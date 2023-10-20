66d52d4443f17d1b6bec89b2063ac0b6afcf8ee1
arsinspace <140083535+arsinspace@users.noreply.github.com>
2023-10-20 14:07:27 +0300

    Rework with Mockito and AssertJ UserServiceImplTest.java

--- src/test/java/UserServiceImplTest.java
+++ src/test/java/UserServiceImplTest.java
@@ -1,68 +1,72 @@
 import config.ContainerEnvironment;
-import org.junit.Assert;
-import org.junit.Test;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import ru.ylab.dao.UserDAO;
 import ru.ylab.model.Credentials;
 import ru.ylab.model.User;
+import ru.ylab.services.UserService;
 import ru.ylab.services.impl.UserServiceImpl;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Tests for UserServiceImpl
  */
 public class UserServiceImplTest extends ContainerEnvironment {
 
-    @Test
-    public void processRegistrationShouldBeReturnValidUser(){
-        UserServiceImpl service = new UserServiceImpl(new UserDAO());
+    private UserService service;
+    private User testUser;
 
-        String testJsonUser = "{\"name\":\"Adam\",\"lastName\":\"Adam\",\"age\":\"29\",\"credentials\"" +
-                ":{\"login\":\"adam\",\"password\":\"123\"}}";
-        User testUser = User.builder()
-                .name("Adam")
-                .lastName("Adam")
-                .age(String.valueOf(29))
-                .credentials(new Credentials("adam","123"))
-                .build();
-
-        Assert.assertEquals(testUser.getCredentials().toString(),
-                service.processRegistration(testJsonUser).getCredentials().toString());
-        Assert.assertEquals(testUser.getName(),service.processRegistration(testJsonUser).getName());
-        Assert.assertEquals(testUser.getLastName(),service.processRegistration(testJsonUser).getLastName());
-        Assert.assertEquals(testUser.getAge(),service.processRegistration(testJsonUser).getAge());
+    @BeforeEach
+    void setUp(){
+        service = new UserServiceImpl(new UserDAO());
+        testUser = mock(User.class);
+        when(testUser.getName()).thenReturn("Adam");
+        when(testUser.getCredentials()).thenReturn(new Credentials("testLogin","123"));
     }
 
     @Test
-    public void processLoginShouldBeTrue(){
-        UserServiceImpl service = new UserServiceImpl(new UserDAO());
-        String testJsonUser = "{\"name\":\"Adam\",\"lastName\":\"Adam\",\"age\":\"29\",\"credentials\"" +
-                ":{\"login\":\"adam\",\"password\":\"123\"}}";
-        service.processRegistration(testJsonUser);
-        String testJsonCredentials = "{\"login\":\"adam\",\"password\":\"123\"}";
-        String adminCredentials = "{\"login\":\"adam\",\"password\":\"123\"}";
+    public void processRegistrationShouldBeReturnValidUser(){
 
-        Assert.assertTrue(service.processLogin(testJsonCredentials));
-        Assert.assertTrue(service.processLogin(adminCredentials));
+        String testJsonUser = "{\"name\":\"TestName\",\"lastName\":\"TestLastName\",\"age\":\"29\",\"credentials\"" +
+                ":{\"login\":\"testLogin\",\"password\":\"123\"}}";
+
+        Assertions.assertThat(service.processRegistration(testJsonUser).getCredentials().toString())
+                .isEqualTo(testUser.getCredentials().toString());
+
+    }
+
+    @Test
+    public void processLoginUserShouldBeTrue(){
+
+        String testJsonCredentials = "{\"login\":\"adam\",\"password\":\"123\"}";
+
+        Assertions.assertThat(service.processLogin(testJsonCredentials)).isTrue();
+    }
+
+    @Test
+    public void processLoginAdminShouldBeTrue(){
+
+        String adminCredentials = "{\"login\":\"admin\",\"password\":\"admin\"}";
+
+        Assertions.assertThat(service.processLogin(adminCredentials)).isTrue();
     }
     @Test
     public void processLoginShouldBeFalse(){
-        UserServiceImpl service = new UserServiceImpl(new UserDAO());
-        String testJsonUser = "{\"name\":\"Adam\",\"lastName\":\"Adam\",\"age\":\"29\",\"credentials\"" +
-                ":{\"login\":\"adam\",\"password\":\"123\"}}";
-        service.processRegistration(testJsonUser);
-        String testJsonCredentials = "{\"login\":\"adam\",\"password\":\"1234\"}";
 
-        Assert.assertFalse(service.processLogin(testJsonCredentials));
+        String testJsonCredentials = "{\"login\":\"adam\",\"password\":\"12348756\"}";
+
+        Assertions.assertThat(service.processLogin(testJsonCredentials)).isFalse();
+
     }
 
     @Test
     public void processLogoutShouldBeTrue(){
-        UserServiceImpl service = new UserServiceImpl(new UserDAO());
-        String testJsonUser = "{\"name\":\"Adam\",\"lastName\":\"Adam\",\"age\":\"29\",\"credentials\"" +
-                ":{\"login\":\"adam\",\"password\":\"123\"}}";
-        service.processRegistration(testJsonUser);
-        String testJsonCredentials = "{\"login\":\"adam\",\"password\":\"123\"}";
-        service.processLogin(testJsonCredentials);
-
-        Assert.assertTrue(service.processLogout());
+        service.processLogin("{\"login\":\"admin\",\"password\":\"admin\"}");
+        service.processLogout();
+        
+        Assertions.assertThat(service.getCurrentAppUser()).isNull();
     }
 }
